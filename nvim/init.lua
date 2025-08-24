0f9b3099c652a4fdfd46ad2100b916f467e6a1b5
rezaqomy <rezaqomyalasl@gmail.com>
2025-08-24 12:22:16 +0330

    init

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmd")
+
+-- Plugins
+require("plugins")
---
+++ nvim/plugins/init.lua
@@ -0,0 +1,18 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- نمونه پلاگین‌ها:
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-tree/nvim-tree.lua" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "neovim/nvim-lspconfig" },
+})
